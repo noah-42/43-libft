721c2044d86984518bf1a8f8fd61e3eb6ba522aa
noah-42 <noahwork42@gmail.com>
2026-05-20 10:46:33 +0200

    headers & ft_strlcpy

---
+++ 43_libft/ft_strlcpy.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: noah     <noah-   @student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*                                                     #+#    #+#             */
+/*                                                    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	_Overview_
+		Copies a string from a source. 'src' to a destination 'dst' —>
+		safely —> meaning if there is any mismatch the program will not
+		crash:
+			- always null-terminates
+		 	- never overflows
+			- returns `strlen(src)` so one can detect truncation.
+
+	_Context_
+		This function is only meant to be used for strings.
+
+	_Parameter(s)_
+		The 'size' parameter is the size of the `dst` string.
+
+	_Return value_
+		The return value is the size of the string that the function TRIES
+		to create (i.e. the number of characters in the `src` parameter).
+
+		As mentioned, the caller of the function can use this to know if the copy
+		of the string was truncated.
+
+	_Risks_
+	- truncated copy from 'src' to 'dst'
+
+*/
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	if (dst_size == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] != '\0' && i < (dst_size - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
+
+/*
++---------+---------+----------+-----------+--------+------------------------+
+| Example |   src   | dst size | dst after | return |       Truncated?       |
++---------+---------+----------+-----------+--------+------------------------+
+|    1    | "hello" |    20    |  "hello"  |   5    |          No            |
++---------+---------+----------+-----------+--------+------------------------+
+|    2    | "hello" |     4    |   "hel"   |   5    |   Yes (5 >= 4)         |
++---------+---------+----------+-----------+--------+------------------------+
+|    3    | "hello" |     0    | unchanged |   5    | Yes (nothing written)  |
++---------+---------+----------+-----------+--------+------------------------+
+|    4    | "hello" |     1    |    ""     |   5    |   Yes (null only)      |
++---------+---------+----------+-----------+--------+------------------------+
+|    5    |    ""   |    10    |    ""     |   0    |          No            |
++---------+---------+----------+-----------+--------+------------------------+
+|    6    | "hello" |     6    |  "hello"  |   5    |   No (exact fit)       |
++---------+---------+----------+-----------+--------+------------------------+
+*/
